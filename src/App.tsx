6b237bdc730684ae5962e5af36bb396e3169c21e
thoratica <me@tica.fun>
2021-11-15 00:03:54 +0900

    Created empty main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import Login from './pages/Login';
+import Main from './pages/Main';
 import store from './store';
 
 const App = observer(() => {
@@ -9,7 +10,7 @@ const App = observer(() => {
 
   return (
     <>
-      {!clientObject.logon ? <Login /> : <div></div>}
+      {!clientObject.logon ? <Login /> : <Main />}
       <Toaster position='bottom-left' reverseOrder={false} />
     </>
   );
---
+++ src/pages/Main.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+
+const Main = observer(() => {
+  return <div className='flex'></div>;
+});
+
+export default Main;
